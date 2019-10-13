0efdd25ba3ccebae127b4e1c6a73ebef0bad68df
Greg <10319786+majorthorn@users.noreply.github.com>
2019-10-12 22:51:12 -0500

    testing done with argstest

--- argstest.sh
+++ argstest.sh
@@ -2,9 +2,25 @@
 
 # set -e
 
-echo "positionals params"
-echo '$0 = ' $0
-echo '$1 = ' $1
-echo '$2 = ' $2
-echo '$3 = ' $3
+print_help() {
+    echo "Usage: bash goinstall.sh OPTIONS"
+    echo -e "\nOPTIONS:"
+    echo -e "  --remove\tRemove currently installed version"
+    echo -e "  --version\tSpecify a version number to install"
+}
+VERSION="1.1"
+echo $VERSION
+if [ "$1" == "--version" ]; then
+    if [ -z "$2" ]; then
+        echo "$1 Requires a Number"
+        
+    else
+        VERSION=$2
+    fi
+elif [ "$1" == "--help" ]; then
+    print_help
+    exit 0
 
+fi
+
+echo $VERSION
